db3f050c56c848e8f6373f6af214f2f05fd70219
K Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>
2025-02-04 16:28:51 +0530

    Format code

--- chitchat/src/main/kotlin/com/sivalabs/chitchat/api/PostMapper.kt
+++ chitchat/src/main/kotlin/com/sivalabs/chitchat/api/PostMapper.kt
@@ -1,23 +1,11 @@
 package com.sivalabs.chitchat.api
 
-import com.sivalabs.chitchat.domain.Post
 import com.sivalabs.chitchat.domain.PostDto
 import com.sivalabs.chitchat.domain.PostProjection
 import org.springframework.stereotype.Component
 
 @Component
 class PostMapper {
-    fun toPostDto(post: Post): PostDto =
-        PostDto(
-            post.id!!,
-            post.uid,
-            post.content,
-            post.createdBy.name,
-            false,
-            post.createdAt,
-            post.updatedAt,
-        )
-
     fun toPostDto(post: PostProjection): PostDto =
         PostDto(
             post.getId(),
